1291512a701599ba8dfaead412c112fe192ad27f
SM SAIF <smsaif767@gmail.com>
2024-01-12 15:06:15 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+// App.js
 import './App.css'
 import React from 'react'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Home />
     </div>
